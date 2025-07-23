f066403b984762ef9c11556436bc4e88d2e21374
Filip2k03 <stephanfilip7@gmail.com>
2025-07-23 14:50:22 +0630

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.logixwhisperx.app',
+  appName: 'Stephan Filip',
+  webDir: 'dist'
+};
+
+export default config;
